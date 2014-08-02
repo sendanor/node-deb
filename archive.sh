1b71e50196db6208465d1d4310763eabefbd3875
Jaakko-Heikki Heusala <jheusala@iki.fi>
2014-08-02 09:03:18 +0300

    Improved archive srcipt

--- archive.sh
+++ archive.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 set -e
-mv build/ tmp/build-"$(date '+%Y%m%d-%H%M%S')"/
-mkdir build
+
+basedir="$(dirname "$(readlink -e "$0")")"
+cd "$basedir"
+
+test -d tmp || mkdir tmp
+test -d build || mkdir build
+
+if find build -maxdepth 0 -empty|grep -q build; then
+	echo 'No need to archive anything.' >&2
+else
+	archive_dir=tmp/build-"$(date '+%Y%m%d-%H%M%S')"
+	echo "Archiving build to $archive_dir ..." >&2
+	mv build/ "$archive_dir"/
+	mkdir build
+fi
+
 # EOF
